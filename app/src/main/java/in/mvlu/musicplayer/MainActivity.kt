4869ae34e947033c9a428513989ac63245c6b947
VaibhavProject-1 <vaibhavpatel003@gmail.com>
2023-03-16 22:46:27 +0530

    Music Player Using Kotlin

---
+++ app/src/main/java/in/mvlu/musicplayer/MainActivity.kt
@@ -0,0 +1,41 @@
+package `in`.mvlu.musicplayer
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnStop: Button
+    lateinit var btnPlay : Button
+    lateinit var btnPause: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initComponent()
+        initClick()
+    }
+    private fun initComponent() {
+        btnStop = findViewById(R.id.btn_stop)
+        btnPlay = findViewById(R.id.btn_play)
+        btnPause = findViewById(R.id.btn_pause)
+    }
+
+    private fun initClick() {
+        val mediaPlayer: MediaPlayer = MediaPlayer.create(applicationContext, R.raw.test)
+
+        btnStop.setOnClickListener{
+            mediaPlayer.stop()
+            mediaPlayer.prepare()
+        }
+
+        btnPlay.setOnClickListener{
+            mediaPlayer.start()
+        }
+
+        btnPause.setOnClickListener{
+            mediaPlayer.pause()
+        }
+    }
+}
